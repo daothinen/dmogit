b737971e2e6df68ecfbe75f1330aff5568085f08
ADMIN <daothinen26000@gmail.com>
2020-11-03 15:36:24 +0700

    Đẩy file 2

--- demogh/Program.cs
+++ demogh/Program.cs
@@ -11,6 +11,7 @@ namespace demogh
         static void Main(string[] args)
         {
             Console.WriteLine("Hello word");
+            Console.WriteLine("Hello word");
         }
     }
 }
